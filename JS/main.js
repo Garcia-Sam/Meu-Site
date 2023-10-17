ef9f58684aa24091b03a2e6edb1ba928af36443b
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-10-17 17:35:22 -0300

    Update index.html, main.js and style.css

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,9 @@
-const   conteudo =   document.querySelector("article")
+const   conteudo =  document.querySelector("#trabalhos")
+const   paragrafo   =   document.createElement('p')
+const   imagem  =   document.createElement('img')
 
-const div = document.createElement("div")
+conteudo.appendChild(paragrafo)
+paragrafo.textContent = 'lorem'
+
+conteudo.appendChild(imagem)
+imagem.setAttribute ('src', './imgs/Projeto-Android.png')
